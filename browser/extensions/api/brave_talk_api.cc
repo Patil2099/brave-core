72019e2af795c2c6aa4afea9ca034446f4ad5fc2
Brian Clifton <brian@clifton.me>
2021-07-28 23:31:48 -0700

    Rename extension API

--- browser/extensions/api/brave_talk_api.cc
+++ browser/extensions/api/brave_talk_api.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/extensions/api/brave_together_api.h"
+#include "brave/browser/extensions/api/brave_talk_api.h"
 
 #include <string>
 #include <memory>
